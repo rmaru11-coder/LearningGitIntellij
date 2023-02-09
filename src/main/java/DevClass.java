98ff00575cc56e67445c63e0898540af8228e2c2
rohanmaru <rohan_engg11@rediffmail.com>
2023-02-09 23:22:54 +0530

    Initial changes in Git|squash and merge

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,6 +2,8 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("Dev class");
         //Created by Rohan Maru
+        //just learning merge and squash
+
     }
     //jknkjbkj
 }

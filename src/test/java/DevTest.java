1b20901ba6785b03a5085800dc7a9192b0f3222b
rmaru11-coder <81004600+rmaru11-coder@users.noreply.github.com>
2023-02-09 23:27:38 +0530

    Initial changes in Git|squash and merge (#2)

--- src/test/java/DevTest.java
+++ src/test/java/DevTest.java
@@ -3,5 +3,6 @@ public class DevTest {
     public static void main(String[] args) {
         System.out.println("Dev class Test");
         //Created by Rohan Maru
+        //just learning merge and squash
     }
 }
